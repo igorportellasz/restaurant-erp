c99515072978e2d9b231f60b87d5ae72c6d38846
Igor Portella <igorportella74@gmail.com>
2025-03-23 19:26:38 -0300

    feat: config eslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import { Button } from './components/ui/button'
 import './global.css'
+
+import { Button } from './components/ui/button'
 
 export function App() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-svh">
+    <div className="flex min-h-svh flex-col items-center justify-center">
       <Button>Enviar</Button>
     </div>
   )
